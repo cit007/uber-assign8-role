e21734d8f4acb952bc17117c1a166646626670d7
cit007 <itcho0077@gmail.com>
2021-01-20 21:07:06 +0900

    global @Role() decorator for authorization

--- src/auth/auth.guard.ts
+++ src/auth/auth.guard.ts
@@ -1,14 +1,28 @@
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
 import { GqlExecutionContext } from '@nestjs/graphql';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
+  constructor(private readonly reflector: Reflector) {}
+
   canActivate(context: ExecutionContext) {
+    const roles = this.reflector.get('roles', context.getHandler());
+    console.log('CanActivie roles: ', roles);
+    if (!roles) {
+      return true;
+    }
+
     const gqlContext = GqlExecutionContext.create(context).getContext();
     const user = gqlContext['user'];
     if (!user) {
       return false;
     }
-    return true;
+
+    if (roles.includes('Any')) {
+      return true;
+    }
+
+    return roles.includes(user.role);
   }
 }
